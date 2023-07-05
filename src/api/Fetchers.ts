be15f6241d6442192700393dd5c7312a2eaacf00
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-07-05 23:03:13 +0900

    Feat: API 이미지 3개씩 받아오도록 수정

--- src/api/Fetchers.ts
+++ src/api/Fetchers.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default async function getDog() {
-  const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+  const response = await axios.get("https://dog.ceo/api/breeds/image/random/3");
   return response.data.message;
 }
